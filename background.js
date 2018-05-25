f1e9738fb9c15d804c3d48a183e7dac1f44136f4
Luis Leon <luis9928@gmail.com>
2018-05-25 12:15:58 -0500

    Deleted
    Deleted

--- background.js
+++
@@ -1,4 +0,0 @@
-// When the extension button/icon is click it opens up the main page
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({url: 'mainPage.html'});
-});
